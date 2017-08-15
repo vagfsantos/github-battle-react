038acede51a914c3e43beb747b6a6ef334c2f727
vagner santos <santos.vagnerferreira@gmail.com>
2017-08-14 20:00:22 -0700

    basic webpack config

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Style from './components/scss/index.scss';
+
+var Hello = () => <div>Hello!</div>;
+
+ReactDOM.render(
+    <Hello />,
+    document.querySelector('#app')
+)
+console.log('okaa')
